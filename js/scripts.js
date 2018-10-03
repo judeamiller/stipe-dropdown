4ef4265370fae644c9178c40bdd6c1f28e2d4600
Jude Baca-Miller <judeamiller@gmail.com>
2018-10-03 16:37:31 -0600

    tutorial complete

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,19 @@ function handleEnter() {
     background.classList.add('open');
 
     const dropdown = this.querySelector('.dropdown');
-    const dropdownCoods = dropdown.getBoundingClientRect();
-    const navCoods = nav.getBoundingClientRect();
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
 
     const coords = {
-        height: dropdownCoods.height,
-        width: dropdownCoods.width
+        height: dropdownCoords.height,
+        width: dropdownCoords.width,
+        top: dropdownCoords.top - navCoords.top,
+        left: dropdownCoords.left - navCoords.left
     }
 
     background.style.setProperty('width', `${coords.width}px`);
     background.style.setProperty('height', `${coords.height}px`);
+    background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px`)
 
 }
 
