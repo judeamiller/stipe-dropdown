dad9ebef993be410d94fd6771e1d5c32f14832b4
Jude Baca-Miller <judeamiller@gmail.com>
2018-10-03 16:29:39 -0600

    add funcitons to capture height and with of dropdowns.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
 const triggers = document.querySelectorAll('.cool > li');
 const background =  document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
+
+function handleEnter() {
+    this.classList.add('trigger-enter');
+    setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+    background.classList.add('open');
+
+    const dropdown = this.querySelector('.dropdown');
+    const dropdownCoods = dropdown.getBoundingClientRect();
+    const navCoods = nav.getBoundingClientRect();
+
+    const coords = {
+        height: dropdownCoods.height,
+        width: dropdownCoods.width
+    }
+
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('height', `${coords.height}px`);
+
+}
+
+function handlLeave() {
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handlLeave));
